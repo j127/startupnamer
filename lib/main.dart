3433eb8cab507be111624765b49cd1cfdc69c36b
J. Cohen <hostelmarketing@gmail.com>
2020-07-19 20:51:43 -0700

    Add ListTiles with infinite scrolling

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,53 @@ void main() => runApp(StartupNamer());
 class StartupNamer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: "Startup Namer App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Welcome to Startup Namer"),
-        ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        )
-      )
+      home: RandomWords()
+    );
+  }
+}
+
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Startup Name Generator"),
+      ),
+      body: _buildSuggestions(),
     );
   }
 }
